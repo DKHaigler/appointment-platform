efc796d5e8b273b384f7600f89898bbe547172ae
DKHaigler <quelsthepoet@gmail.com>
2026-09-06 19:38:13 -0400

    feat: built landing page

---
+++ supabase/migrations/20260906224131_create_profile_on_signup.sql
@@ -0,0 +1,15 @@
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer
+as $$
+begin
+    insert into public.profiles (id)
+    values (new.id);
+end;
+$$;
+
+create trigger new_user
+after insert on auth.users
+for each row
+execute function public.handle_new_user();
